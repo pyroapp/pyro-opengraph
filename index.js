7db880cf6f21f0ec36fb5e25e5891bd9b268130b
Cooper Beltrami <45991010+glowbase@users.noreply.github.com>
2021-05-06 17:08:51 +1000

    REF: Cleaned code, changed request to take query strings rather than xxurlencoded body

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
-const opengraph = require('open-graph')
-const express = require('express')
-const bodyParser = require('body-parser');
+const opengraph = require('open-graph');
+const express = require('express');
 
-const app = express();
+const api = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
-
-app.get('/', async (req, res) => {
+api.get('/', async (req, res) => {
     try {
-        const { url } = req.body
+        const { url } = req.query;
+
+        if (!url) return res.status(400).json({
+            success: false,
+            error: 'Failed to provide url'
+        });
+
         opengraph(url, (err, meta) => {
-            console.log(meta)
             res.status(200).json({
                 success: true,
                 data: meta
-            })
-        })
+            });
+        });
     } catch (error) {
-        res.status(418).json({
+        res.status(400).json({
             success: false,
             error: error.message,
-            usage: "URL",
-            sent: req.body
-        })
-
+        });
     }
-})
+});
 
-app.listen(8080, () => {
-	console.log('Uploader listning on port 8080')
+api.listen(8080, () => {
+    console.clear();
+	console.log('LISTENING ON PORT 8080');
 });
